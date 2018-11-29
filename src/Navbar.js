aed0e1b0ebe57537a9f4bc8ff867c11d12c9ce3a
Uche Ozoemena <ucheozoemena@gmail.com>
2018-11-29 17:07:28 +0100

    Create Navbar component

---
+++ src/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav
+      className="ui inverted fluid one item menu"
+    ><h1 className="item">Fake Famous!</h1></nav>
+  );
+};
+
+export default Navbar;
