4cc7fc2a366e6e34ba3619380d887cf67f23bbe0
Uche Ozoemena <ucheozoemena@gmail.com>
2018-11-30 07:06:58 +0100

    Generate avatar with faker

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Navbar from './Navbar';
 import Image from './Image';
+import faker from 'faker';
 
 const App = () => {
   return (
@@ -9,7 +10,7 @@ const App = () => {
       <Navbar />
       <section className="person">
         <section className="ui card">
-          <Image pic="test.jpg" />
+          <Image pic={faker.image.avatar()} />
         </section>
       </section>
     </div>
