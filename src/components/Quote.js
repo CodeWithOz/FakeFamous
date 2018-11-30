c6dc8e8463eb23991995713bbcbb67a96bcfcb12
Uche Ozoemena <ucheozoemena@gmail.com>
2018-11-30 15:12:25 +0100

    Increase font-size of the quote

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const Quote = props => {
-  return <p className="center aligned quote">" {props.words} "</p>;
+  return (
+    <p
+      className="center aligned quote"
+      style={{
+        fontSize: '1.8em'
+      }}
+    >"{props.words}"</p>
+  );
 };
 
 export default Quote;
