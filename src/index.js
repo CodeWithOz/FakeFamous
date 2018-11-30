41d2f7a70dd594958d39c6458e9342be5336748a
Uche Ozoemena <ucheozoemena@gmail.com>
2018-11-30 06:50:33 +0100

    Render the Image component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Navbar from './Navbar';
+import Image from './Image';
 
 const App = () => {
   return (
-    <Navbar />
+    <div>
+      <Navbar />
+      <section className="person">
+        <section className="ui card">
+          <Image pic="test.jpg" />
+        </section>
+      </section>
+    </div>
   );
 };
 
