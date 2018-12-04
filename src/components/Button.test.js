5d5f5fad650acf2658374331c7c63e52cff74d15
Uche Ozoemena <ucheozoemena@gmail.com>
2018-12-04 17:48:31 +0100

    Test Button component

---
+++ src/components/Button.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Button from './Button';
+import renderer from 'react-test-renderer';
+
+test('Button component exists', () => {
+  const button = renderer.create(
+    <Button
+      onClick={() => console.log('Dummy handler')}
+      color='green'
+      text='Dummy button'
+    />
+  );
+
+  // render the component
+  const tree = button.toJSON();
+  expect(tree).toMatchSnapshot();
+});
